d2dd20cdba424d35a85562cc21cad6cf587f8963
miki2123j <127264981+miki2123j@users.noreply.github.com>
2023-11-11 14:52:05 -0500

    NEW uPDATE

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,35 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // Get the token from the request headers or query parameters
+  const token = req.headers['x-access-token'] || req.query.token;
+
+  // Check if token exists
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Access denied. Token is required.' });
+  }
+
+  // Verify the token
+  jwt.verify(token, 'your_secret_key', function (err, decoded) {
+    if (err) {
+      return res.status(401).json({ success: false, message: 'Failed to authenticate token.' });
+    }
+
+    // Save the decoded user information in the request object for further use
+    req.user = decoded;
+
+    // Continue to the next middleware or route handler
+    next();
+  });
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
+
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,66 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  books.push({"author": req.query.authour,"title": req.query.title, "reviews": req.query.reviews})
+  return res.status(200).json({message: "Book has been added", books});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params.isbn;
+  let filtered_book = books.filter((book) => book.isbn === isbn);
+
+  if (!filtered_book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.status(200).send(JSON.stringify({filtered_book},null,4));
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { author } = req.params.author;
+  let filtered_book = books.filter((book) => book.author === author);
+
+  if(!filtered_book){
+    return res.status(404).json({message:"Author not found"});
+  }
+  return res.status(200).send(JSON.stringify({filtered_book},null,4));
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { title } = req.params.title;
+  let filtered_book = books.filter((book) => book.title === title);
+
+  if(!filtered_book){
+    return res.status(404).json({message:"Title not found"});
+  }
+  return res.status(200).send(JSON.stringify({filtered_book},null,4));
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params.isbn;
+  let filtered_reviews = reviews.filter((review) => review.isbn === isbn);
+  if(!filtered_reviews){
+    return res.status(404).json({message:"Review not found"});
+  }
+  return res.status(200).send(JSON.stringify({filtered_reviews},null,4));
+
 });
 
 module.exports.general = public_users;
